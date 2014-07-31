fac8c63365c80d72784338215c6700f25f04ffc9
Alexander Perov <alexander.perov@gmail.com>
2014-08-01 01:19:53 +0400

    init

---
+++ src/Sb/Utils.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Sb;
+
+class Utils
+{
+    public static function getNameMany($name)
+    {
+        $result = $name;
+        $lastWord = substr($name, -1, 1);
+
+        switch ($lastWord) {
+            case 's':
+                $result .= 'es';
+                break;
+            case 'y':
+                $result = substr($result, 0, strlen($result) - 1) . 'ies';
+                break;
+            default:
+                $result .= 's';
+        }
+        return $result;
+    }
+
+    public static function wordUnderscoreToCamelCase($value)
+    {
+        $separator = '_';
+        $isUnicodeSupportEnabled = (@preg_match('/\pL/u', 'a')) ? true : false;
+
+        $pregQuotedSeparator = preg_quote($separator, '#');
+
+        if ($isUnicodeSupportEnabled) {
+            $matchPattern = array('#('.$pregQuotedSeparator.')(\p{L}{1})#e','#(^\p{Ll}{1})#e');
+            $replacement = array("strtoupper('\\2')","strtoupper('\\1')");
+        } else {
+            $matchPattern = array('#('.$pregQuotedSeparator.')([A-Za-z]{1})#e','#(^[A-Za-z]{1})#e');
+            $replacement = array("strtoupper('\\2')","strtoupper('\\1')");
+        }
+
+        return lcfirst(preg_replace($matchPattern, $replacement, $value));
+    }
+
+}
