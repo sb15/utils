7724d4ddb3c938c845cd9df876743af90df52cf5
Alexander Perov <alexander.perov@gmail.com>
2014-09-15 00:02:40 +0400

    update

--- src/Utils.php
+++ src/Utils.php
@@ -104,4 +104,16 @@ class Utils
         return round($matchPercent, 2);
     }
 
+    public static function getDate($now = null)
+    {
+        if ((is_null($now))) {
+            return date("Y-m-d H:i:s");
+        } elseif (is_numeric($now)) {
+            return date("Y-m-d H:i:s", $now);
+        } elseif (is_string($now)) {
+            return date("Y-m-d H:i:s", strtotime($now));
+        }
+        return null;
+    }
+
 }
