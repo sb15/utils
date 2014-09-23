b4ea43e392eee815588e2aa3ded3a1f77220bd45
Alexander Perov <alexander.perov@gmail.com>
2014-09-23 16:55:47 +0400

    plural ru

--- src/Utils.php
+++ src/Utils.php
@@ -115,5 +115,11 @@ class Utils
         }
         return null;
     }
+    
+    public static function pluralRu($number, $titles)
+    {
+        $cases = array (2, 0, 1, 1, 1, 2);
+        return $titles[ ($number%100>4 && $number%100<20)? 2 : $cases[min($number%10, 5)] ];
+    }
 
 }
